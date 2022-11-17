79d3d1a3071bce7b11b13307d4e658dc3dc218c1
Deifson Oliveira <nosfied@gmail.com>
2022-11-16 21:13:42 -0300

    cookies para tjdf

--- src/models/tjdfModel.js
+++ src/models/tjdfModel.js
@@ -27,6 +27,31 @@ puppeteer.use(
     })
 )
 
+async function pegarCookies(sitio){
+
+    //implementação e credenciais bright data
+    const cookieJar = request.jar();
+    request = request.defaults({jar: cookieJar});
+    var username = 'lum-customer-hl_31c0867f-zone-unblocker';
+    var password = 'f8fx0rhf0tue';
+    var port = 22225;
+    var user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+    var session_id = (1000000 * Math.random())|0;
+    var super_proxy = 'http://'+username+'-country-br-session-'+session_id+':'+password+'@zproxy.lum-superproxy.io:'+port;
+    var options = {
+        url: 'https://www.tjdft.jus.br/',
+        proxy: super_proxy,
+        rejectUnauthorized: false,
+        headers: {'User-Agent': user_agent}
+    };
+
+    let cookies = await request(options)
+    .then(function(data){ console.log("ok, pagina da url"); },
+        function(err){ console.error(err); });
+        let cookiesTrf3 = cookieJar.getCookieString('https://www.tjdft.jus.br/');
+        return cookiesTrf3;
+}
+
 exports.tjdf = async (dados) => {    
 
     console.log("TJDF Processando...");
@@ -44,7 +69,7 @@ exports.tjdf = async (dados) => {
         ignoreHTTPSErrors: true        
     
     });
-    let cookie = await util.pegarCookies(sitioParaCookies);
+    let cookie = await pegarCookies();
     const cookies = [{name: 'cookie', value: `${cookie}`, domain: 'https://cnc.tjdft.jus.br/solicitacao-externa'}];
     console.log(cookies);
 
