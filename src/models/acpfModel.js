23472fc29de7bcb7cb898a60ad6947ceda4452a1
Deifson Oliveira <nosfied@gmail.com>
2023-03-24 15:14:00 -0300

    atualização em ACPF5

--- src/models/acpfModel.js
+++ src/models/acpfModel.js
@@ -88,7 +88,7 @@ exports.acpf = async (dados) => {
                     console.log("ACPF: Processo interrompido pelo Captcha. Tentando solucionar...");            
                     let quebrarCaptcha = await page.solveRecaptchas();
                     console.log(quebrarCaptcha);
-                    await page.waitForTimeout(12000);
+                    await page.waitForTimeout(12000000);
                     await page.click('body > div.wrapper.ng-scope > application > div > certidao > div > div:nth-child(2) > div > div > div.panel-body > form > div > div.form-group.form-group-sm > div > button.btn.btn-primary.btn-sm', { delay: 3000 });
                 }else{
                     await page.click('body > div.wrapper.ng-scope > application > div > certidao > div > div:nth-child(2) > div > div > div.panel-body > form > div > div.form-group.form-group-sm > div > button.btn.btn-primary.btn-sm', { delay: 3000 });
