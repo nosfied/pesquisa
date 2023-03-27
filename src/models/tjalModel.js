104d564d43fc5ddfe41d4cb8a5b887d8a9134cf5
Deifson Oliveira <nosfied@gmail.com>
2023-03-27 16:19:34 -0300

    ajuste em tjal

--- src/models/tjalModel.js
+++ src/models/tjalModel.js
@@ -105,9 +105,10 @@ exports.tjal = async (dados) => {
                     console.log("TJAL: Processo interrompido pelo Captcha. Tentando solucionar...");            
                     let quebrarCaptcha = await page.solveRecaptchas();
                     console.log(quebrarCaptcha);
-                    await page.waitForTimeout(2000);
-                    await page.click('#confirmacaoInformacoes', {delay:2000});
-                    await page.click('#pbEnviar', {delay:1000});
+                    await page.waitForTimeout(4000);
+                    await page.click('#confirmacaoInformacoes', {delay:4000});
+                    await page.waitForTimeout(4000);
+                    await page.click('#pbEnviar', {delay:3000});
                     await page.waitForTimeout(3000);
                     let atencao = await page.$('body > div.blockUI.blockMsg.blockPage');
                     if(atencao){
@@ -120,7 +121,11 @@ exports.tjal = async (dados) => {
                     await page.click('#pbEnviar', {delay:1000});
                     await page.waitForTimeout(20000);
                 }
-
+                let atencao2 = await page.$('body > div.blockUI.blockMsg.blockPage');
+                if(atencao2){
+                    await page.click('#btnSim', {delay:1000});
+                }
+                await page.waitForTimeout(6000);
                 let nPedido = await page.$eval('body > table:nth-child(4) > tbody > tr > td > form > div:nth-child(2) > table.secaoFormBody > tbody > tr:nth-child(1) > td:nth-child(2) > span', el => el.textContent);
                 let dtPedido = await page.$eval('body > table:nth-child(4) > tbody > tr > td > form > div:nth-child(2) > table.secaoFormBody > tbody > tr:nth-child(2) > td:nth-child(2) > span', el => el.textContent);
                 let cpf = await page.$eval('body > table:nth-child(4) > tbody > tr > td > form > div:nth-child(3) > table.secaoFormBody > tbody > tr:nth-child(4) > td:nth-child(2) > table > tbody > tr > td > span:nth-child(1) > span', el => el.textContent);
@@ -134,21 +139,24 @@ exports.tjal = async (dados) => {
                 
                 await util.limparArquivosAntigos();
                 await page.goto(SITE_URL2, {waitUntil: 'networkidle2'});
-                await page.waitForTimeout(3000);
-                await page.click('#cdModelo', { delay: 2000 });
+                await page.waitForTimeout(2000);
+                await page.click('#tipo_modelo', { delay: 2000 });
                 if(tipo == 'criminal2'){
                     await page.keyboard.press('ArrowDown', {delay:1000});
                     await page.keyboard.press('ArrowDown', {delay:1000});
+                    await page.keyboard.press('ArrowDown', {delay:1000});
                 }else{
+                    await page.keyboard.press('ArrowDown', {delay:1000});
                     await page.keyboard.press('ArrowDown', {delay:1000});
                 }                
                 await page.keyboard.press('Enter', {delay:1000});            
                 await page.keyboard.press('Tab', {delay:3000});            
                 await page.keyboard.press('Tab', {delay:3000});
                 await page.keyboard.type(NOME,{delay:150});
-                await page.keyboard.press('Tab', {delay:1000});            
+                await page.keyboard.press('Tab', {delay:1000});
+                await page.keyboard.press('Tab', {delay:1000});
                 await page.keyboard.type(CPF,{delay:150});
-                await page.keyboard.press('Tab', {delay:1000});            
+                await page.keyboard.press('Tab', {delay:1000});
                 await page.keyboard.type(RG,{delay:150});
                 if(SEXO == 'masculino'){
                     await page.keyboard.press('Tab', {delay:1000});
