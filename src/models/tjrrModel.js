3a98993fb5cb3642634803c22319926c65d444cd
Deifson Oliveira <nosfied@gmail.com>
2023-03-27 22:22:12 -0300

    ajuste em tjrr

--- src/models/tjrrModel.js
+++ src/models/tjrrModel.js
@@ -78,6 +78,7 @@ exports.tjrr = async (dados) => {
                     await page.waitForTimeout(2000);
                     //await page.click('#cpf', {delay:2000});
                     await page.click('#form > div > div > div > a', {delay:2000});
+                    await page.focus('#form > div > div > div > a', {delay:1000});
                     await page.click('#form > div > div > div > a', {delay:1000});
                     await page.waitForTimeout(10000);                            
                 }else{
