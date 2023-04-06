7f0721292f08977a45507385e89b329dd1c46d9e
Deifson Oliveira <nosfied@gmail.com>
2023-04-06 10:23:58 -0300

    ajustes gerais

--- src/models/tjspModel.js
+++ src/models/tjspModel.js
@@ -43,7 +43,7 @@ exports.tjsp = async (dados) => {
     const browser = await puppeteer.launch({
         headless: false,
         executablePath: paths.googleChrome(),
-        userDataDir: paths.perfilChrome(),
+        //userDataDir: paths.perfilChrome(),
         defaultViewport: false,
         ignoreHTTPSErrors: true        
     
