52aa17f9b411b0adaedde9466ef06af3b96a8d48
Deifson Oliveira <nosfied@gmail.com>
2023-04-12 11:09:31 -0300

    add middleware checkCsrfError em todas a rotas

--- routes.js
+++ routes.js
@@ -17,45 +17,45 @@ route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
 
 //Rotas da Pesquisa
-route.post('/pesquisa/trf1', loginRequired, pesquisaController.trf1);
-route.post('/pesquisa/trf2', loginRequired, pesquisaController.trf2);
-route.post('/pesquisa/trf3', loginRequired, pesquisaController.trf3);
-route.post('/pesquisa/trf4', loginRequired, pesquisaController.trf4);
-route.post('/pesquisa/trf5', loginRequired, pesquisaController.trf5);
-route.post('/pesquisa/tjdf', loginRequired, pesquisaController.tjdf);
-route.post('/pesquisa/tjms', loginRequired, pesquisaController.tjms);
-route.post('/pesquisa/tjsp', loginRequired, pesquisaController.tjsp);
-route.post('/pesquisa/tjsc', loginRequired, pesquisaController.tjsc);
-route.post('/pesquisa/tjrs', loginRequired, pesquisaController.tjrs);
-route.post('/pesquisa/tjba', loginRequired, pesquisaController.tjba);
-route.post('/pesquisa/tjpi', loginRequired, pesquisaController.tjpi);
-route.post('/pesquisa/tjal', loginRequired, pesquisaController.tjal);
-route.post('/pesquisa/tjap', loginRequired, pesquisaController.tjap);
-route.post('/pesquisa/tjce', loginRequired, pesquisaController.tjce);
-route.post('/pesquisa/tjes', loginRequired, pesquisaController.tjes);
-route.post('/pesquisa/tjgo', loginRequired, pesquisaController.tjgo);
-route.post('/pesquisa/tjrr', loginRequired, pesquisaController.tjrr);
-route.post('/pesquisa/tjto', loginRequired, pesquisaController.tjto);
-route.post('/pesquisa/tjro', loginRequired, pesquisaController.tjro);
-route.post('/pesquisa/tjam', loginRequired, pesquisaController.tjam);
-route.post('/pesquisa/tjac', loginRequired, pesquisaController.tjac);
-route.post('/pesquisa/tjmt', loginRequired, pesquisaController.tjmt);
-route.post('/pesquisa/tjpe', loginRequired, pesquisaController.tjpe);
-route.post('/pesquisa/tjma', loginRequired, pesquisaController.tjma);
-route.post('/pesquisa/tjse', loginRequired, pesquisaController.tjse);
-route.post('/pesquisa/tjrn', loginRequired, pesquisaController.tjrn);
-route.post('/pesquisa/tjpb', loginRequired, pesquisaController.tjpb);
-route.post('/pesquisa/tjpa', loginRequired, pesquisaController.tjpa);
+route.post('/pesquisa/trf1', checkCsrfError, loginRequired, pesquisaController.trf1);
+route.post('/pesquisa/trf3', checkCsrfError, loginRequired, pesquisaController.trf3);
+route.post('/pesquisa/trf2', checkCsrfError, loginRequired, pesquisaController.trf2);
+route.post('/pesquisa/trf4', checkCsrfError, loginRequired, pesquisaController.trf4);
+route.post('/pesquisa/trf5', checkCsrfError, loginRequired, pesquisaController.trf5);
+route.post('/pesquisa/tjdf', checkCsrfError, loginRequired, pesquisaController.tjdf);
+route.post('/pesquisa/tjms', checkCsrfError, loginRequired, pesquisaController.tjms);
+route.post('/pesquisa/tjsp', checkCsrfError, loginRequired, pesquisaController.tjsp);
+route.post('/pesquisa/tjsc', checkCsrfError, loginRequired, pesquisaController.tjsc);
+route.post('/pesquisa/tjrs', checkCsrfError, loginRequired, pesquisaController.tjrs);
+route.post('/pesquisa/tjba', checkCsrfError, loginRequired, pesquisaController.tjba);
+route.post('/pesquisa/tjpi', checkCsrfError, loginRequired, pesquisaController.tjpi);
+route.post('/pesquisa/tjal', checkCsrfError, loginRequired, pesquisaController.tjal);
+route.post('/pesquisa/tjap', checkCsrfError, loginRequired, pesquisaController.tjap);
+route.post('/pesquisa/tjce', checkCsrfError, loginRequired, pesquisaController.tjce);
+route.post('/pesquisa/tjes', checkCsrfError, loginRequired, pesquisaController.tjes);
+route.post('/pesquisa/tjgo', checkCsrfError, loginRequired, pesquisaController.tjgo);
+route.post('/pesquisa/tjrr', checkCsrfError, loginRequired, pesquisaController.tjrr);
+route.post('/pesquisa/tjto', checkCsrfError, loginRequired, pesquisaController.tjto);
+route.post('/pesquisa/tjro', checkCsrfError, loginRequired, pesquisaController.tjro);
+route.post('/pesquisa/tjam', checkCsrfError, loginRequired, pesquisaController.tjam);
+route.post('/pesquisa/tjac', checkCsrfError, loginRequired, pesquisaController.tjac);
+route.post('/pesquisa/tjmt', checkCsrfError, loginRequired, pesquisaController.tjmt);
+route.post('/pesquisa/tjpe', checkCsrfError, loginRequired, pesquisaController.tjpe);
+route.post('/pesquisa/tjma', checkCsrfError, loginRequired, pesquisaController.tjma);
+route.post('/pesquisa/tjse', checkCsrfError, loginRequired, pesquisaController.tjse);
+route.post('/pesquisa/tjrn', checkCsrfError, loginRequired, pesquisaController.tjrn);
+route.post('/pesquisa/tjpb', checkCsrfError, loginRequired, pesquisaController.tjpb);
+route.post('/pesquisa/tjpa', checkCsrfError, loginRequired, pesquisaController.tjpa);
 //Polícia Federal
-route.post('/pesquisa/acpf', loginRequired, pesquisaController.acpf);
+route.post('/pesquisa/acpf', checkCsrfError, loginRequired, pesquisaController.acpf);
 //TSE
-route.post('/pesquisa/ctse', loginRequired, pesquisaController.ctse);
+route.post('/pesquisa/ctse', checkCsrfError, loginRequired, pesquisaController.ctse);
 //TCU
-route.post('/pesquisa/ctcu', loginRequired, pesquisaController.ctcu);
+route.post('/pesquisa/ctcu', checkCsrfError, loginRequired, pesquisaController.ctcu);
 //Receita Federal
-route.post('/pesquisa/crf', loginRequired, pesquisaController.crf);
+route.post('/pesquisa/crf', checkCsrfError, loginRequired, pesquisaController.crf);
 //STM
-route.post('/pesquisa/stm', loginRequired, pesquisaController.stm);
+route.post('/pesquisa/stm', checkCsrfError, loginRequired, pesquisaController.stm);
 
 route.get('/pesquisa/enviada/:email', loginRequired, pesquisaController.certidaoEnviada);
 route.get('/pesquisa/files/:diretorio/:cpf/:orgao', pesquisaController.download);
